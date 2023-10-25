2cfc03b010e5753ebd7d26807f8b6e31174ad0bb
ponybo-y <98256435+ponybo-y@users.noreply.github.com>
2023-10-25 15:15:55 +0530

    added sorting algorithms to the Algorithms folder

---
+++ Algorithms/sorting-algorithms/shell-sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void shellSort(vector<int>& arr) {
+    int n = arr.size();
+
+    // Start with a big gap, then reduce the gap
+    for (int gap = n/2; gap > 0; gap /= 2) {
+        // Do a gapped insertion sort for this gap size.
+        // The first gap elements a[0..gap-1] are already in gapped order
+        // keep adding one more element until the entire array is gap sorted
+        for (int i = gap; i < n; i++) {
+            // add a[i] to the elements that have been gap sorted
+            // save a[i] in temp and make a hole at position i
+            int temp = arr[i];
+
+            // shift earlier gap-sorted elements up until the correct location for a[i] is found
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+
+            // put temp (the original a[i]) in its correct location
+            arr[j] = temp;
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {12, 34, 54, 2, 3};
+    shellSort(arr);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
